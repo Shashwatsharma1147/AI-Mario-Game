ce7724b456b84a5f28c4622e944652fa81da870d
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-09-14 17:44:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on("pose",getPoses);
+}
+function modelLoaded(){
+	console.log("poseNet is initialised");
+}
+function getPoses(result){
+	if (result.length > 0){
+		//console.log(result);
+		noseX = result[0].pose.nose.x;
+		noseY = result[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game();
+}
+
+
+
+
+
+
